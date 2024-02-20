9a9edb6acea543793951a75673e4190cea76fda0
ShrutiVellanki <shruti.vellanki@mail.utoronto.ca>
2024-02-20 12:34:39 -0500

    Better look for ErrorMessage and finished docs

--- src/stories/messages/ErrorMessage/ErrorMessage.tsx
+++ src/stories/messages/ErrorMessage/ErrorMessage.tsx
@@ -1,30 +1,38 @@
-import PropTypes from 'prop-types';
 import React from 'react';
+import PropTypes from 'prop-types';
 import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+import ErrorIcon from '@mui/icons-material/Error'; // Material-UI error icon
+import { useTheme } from '@mui/material/styles';
 
 /**
- * Component for displaying error messages with an icon and description.
+ * Component for displaying error messages with an optional icon and description.
  */
-export const ErrorMessage = ({
-  icon, // Expecting an MUI Icon or similar
-  description, // Text description of the error
-  ...props
-}) => {
+export const ErrorMessage = ({ icon, description }) => {
+  const theme = useTheme();
+
   return (
-    <div className="company-error-message" {...props}>
-      {/* Display the icon */}
-      {icon && <div>{icon}</div>}
-      {/* Display the description */}
-      <Typography variant="body1">{description}</Typography>
-    </div>
+    <Box display="flex" justifyContent="center" alignItems="center" flexDirection="column" minHeight="100%" p={4}>
+      {icon && (
+        <Box color={theme.palette.error.main} mb={2}>
+          {icon ? icon : <ErrorIcon style={{ fontSize: 60 }} />}
+        </Box>
+      )}
+      <Typography variant="body1" align="center">
+        {description}
+      </Typography>
+    </Box>
   );
 };
 
 ErrorMessage.propTypes = {
-  icon: PropTypes.node, // Icon component
-  description: PropTypes.string.isRequired, // Error description
+  icon: PropTypes.node, // Custom icon component, defaults to ErrorIcon if not provided
+  description: PropTypes.string.isRequired, // Error description text
+  showIcon: PropTypes.bool, // Whether to show the error icon
 };
 
 ErrorMessage.defaultProps = {
-  icon: null, // No default icon
+  showIcon: true, // Show the error icon by default
 };
+
+export default ErrorMessage;
